33e3564c8d594db120be689591c3befd67199b6f
Luki Ramadon <luki.ramadon@pinhome.id>
2022-03-31 15:34:31 +0700

    first

---
+++ MainProj.java
@@ -0,0 +1,9 @@
+import libs.FirstLib;
+
+class MainProj{
+    public static void main(String[] xx){
+        System.out.println("test from main");
+        FirstLib fl = new FirstLib();
+        fl.helloLib();
+    }
+}
